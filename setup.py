5a3530601070b4f46c64d7dc397e952a1497b870
Costas Basdekis <costas.basdekis@gmail.com>
2021-02-03 14:48:25 +0000

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='aox',
-    version='0.1',
+    version='1.0',
     description='AdventOfCode Submissions Framework',
     author='Costas Basdekis',
     author_email='costas@basdekis.io',
