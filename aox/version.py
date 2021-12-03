811dcba4e84cb67197246fa89b26dd84750a9ba7
Costas Basdekis <costas.basdekis@gmail.com>
2021-12-03 03:56:09 +0000

    Always use the full version

--- aox/version.py
+++ aox/version.py
@@ -1,10 +1,6 @@
 AOX_VERSION = (1, 0, 2)
 AOX_VERSION_LABEL = ".".join(map(str, AOX_VERSION))
-AOX_PACKAGE_VERSION_LABEL = (
-    AOX_VERSION_LABEL
-    if any(AOX_VERSION[2:]) else
-    ".".join(map(str, AOX_VERSION[:2]))
-)
+AOX_PACKAGE_VERSION_LABEL = AOX_VERSION_LABEL
 
 if __name__ == '__main__':
     print(AOX_PACKAGE_VERSION_LABEL)
